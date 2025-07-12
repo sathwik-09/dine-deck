bc10391451b4bca08d5395f0d9b4fde934ddc1f9
sathwik <enapothulasathwik09@gmail.com>
2025-07-12 23:05:14 +0530

    feat: get vendor list

--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 router.post("/register", vendorController.venderRegister);
 router.post("/login", vendorController.vendorLogin);
+router.get("/all-vendors", vendorController.getAllVendors);
+router.get("/single-vendor/:id", vendorController.getVendorById);
 
 module.exports = router;
