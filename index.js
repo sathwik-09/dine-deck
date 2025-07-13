6a8a754c11b49fcead8816f50ac64ef9f235021b
sathwik <enapothulasathwik09@gmail.com>
2025-07-13 22:29:44 +0530

    Set up Express server with MongoDB and routes

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const vendorRoutes = require("./routes/vendorRoutes");
+const bodyParser = require("body-parser");
+const firmRoutes = require("./routes/firmRoutes");
+
+const app = express();
+app.use(express.json());
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log("Connected to MongoDB");
+})
+.catch((err)=>{
+    console.log(err);
+});
+app.use(bodyParser.json());
+app.use("/vendor", vendorRoutes); 
+app.use("/firm", firmRoutes);
+
+
+app.listen(3000);
+
+app.use("/home", (req,res)=>{
+    res.send("<h1>Hello World</h1>")
+}) 
