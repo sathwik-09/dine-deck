5054c9da93fa5e4d6683095757be2e0d9fbe7de4
sathwik <enapothulasathwik09@gmail.com>
2025-07-15 23:05:01 +0530

    update route

--- routes/firmRoutes.js
+++ routes/firmRoutes.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const firmController = require("../controllers/firmController");
 const router = express.Router();
-const verifyToken = require("../middlewares/verifyToken");
-
+const verifyToken = require("../middlewares/verifyToken").default;
 
 router.post("/add-firm", verifyToken, firmController.addFirm);
 
