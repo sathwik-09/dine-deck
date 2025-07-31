608cf82ceb3848a702642f0f3c82bb839c7043aa
sathwik <enapothulasathwik09@gmail.com>
2025-07-31 21:59:10 +0530

    add product routes

--- index.js
+++ index.js
@@ -4,24 +4,26 @@ const mongoose = require("mongoose");
 const vendorRoutes = require("./routes/vendorRoutes");
 const bodyParser = require("body-parser");
 const firmRoutes = require("./routes/firmRoutes");
+const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 app.use(express.json());
 dotenv.config();
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>{
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
     console.log("Connected to MongoDB");
-})
-.catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 app.use(bodyParser.json());
-app.use("/vendor", vendorRoutes); 
+app.use("/vendor", vendorRoutes);
 app.use("/firm", firmRoutes);
-
+app.use("/product", productRoutes);
 
 app.listen(3000);
 
-app.use("/home", (req,res)=>{
-    res.send("<h1>Hello World</h1>")
-}) 
+app.use("/home", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
